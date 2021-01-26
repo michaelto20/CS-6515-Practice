24451b570a6da08b74d9c4edacdc19c5713b22cb
Savage <crsavage0630@gmail.com>
2021-01-25 22:01:22 -0500

    Added MaxSumExample

--- CountThree/CountThree/Program.cs
+++ CountThree/CountThree/Program.cs
@@ -1,37 +1,17 @@
 ﻿using System;
 
-namespace Fibonacci
+namespace CountThree
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Fib(6));
+            Console.WriteLine(CountThree());
         }
 
-        private static int Fib(int fibNum)
+        private static int CountThree()
         {
-            if (fibNum == 0)
-            {
-                return 0;
-            }
-            else if (fibNum == 1)
-            {
-                return 1;
-            }
-            else
-            {
-                // Generate Fibonnaci sequence
-                int[] fibs = new int[fibNum + 1];
-                fibs[0] = 0;
-                fibs[1] = 1;
-                for (int i = 2; i <= fibNum; i++)
-                {
-                    fibs[i] = fibs[i - 1] + fibs[i - 2];
-                }
-
-                return fibs[fibNum];
-            }
+            return -1;
         }
     }
 }
---
+++ MaxSum/MaxSum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace MaxSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = new int[] { 5, 8, 3, 7, 9, 1 };
+            // Answer is 3: 5,7,9
+            Console.WriteLine(MaxSum(sequence));
+        }
+
+        private static int MaxSum(int [] sequence)
+        {
+            int[] T = new int[sequence.Length];
+            T[0] = sequence[0];
+            for (int i = 0; i < sequence.Length; i++)
+			{
+                T[i] = sequence[i];
+                for(int j = 0; j <= i; j++)
+                {
+
+                    Console.WriteLine("j position: "+sequence[j]);
+                    if(sequence[j] < sequence[i])
+                    {
+                        T[i] = sequence[i] +Math.Max(T[i-1], 0);
+                    }
+                }
+			}
+            return T.Max();
+        }
+    }
+}
