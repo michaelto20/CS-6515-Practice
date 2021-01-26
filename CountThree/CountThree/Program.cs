b6ee4b25f6fba3a049a8079442593d9385087306
Savage <crsavage0630@gmail.com>
2021-01-26 00:22:47 -0500

    push local

--- CountThree/CountThree/Program.cs
+++ CountThree/CountThree/Program.cs
@@ -8,13 +8,16 @@ namespace CountThree
     {
         static void Main(string[] args)
         {
-            int[] A = new int[] { 10, 13, 7, 8, 14, 11 };
+            //int[] A = new int[] { 10, 13, 7, 8, 14, 11 };
             //Solution: [10,13,14,11], [10,13,11], [10,11], [10,7,8,11], [10,8,11]
+            int[] A = new int[] { 10, 13, 7, 8, 14, 11, 16, 18 };
             //int[] A = new int[] { 10, 12 };
             Console.WriteLine(CountThree(A));
         }
 
-        private static int CountThree(int [] A)
+
+
+        private static int CountThree(int[] A)
         {
             int[] T = new int[A.Length];
             T[0] = 0;
@@ -24,10 +27,10 @@ namespace CountThree
             {
                 T[0] = 1;
             }
-            for (int i = 1; i < A.Length-1; i++)
+            for (int i = 1; i < A.Length - 1; i++)
             {
                 int numThreeChains = 0;
-                
+
                 if (Math.Abs(fElm - A[i]) <= 3)
                 {
                     if (Math.Abs(lElm - A[i]) <= 3)
@@ -35,7 +38,7 @@ namespace CountThree
                         numThreeChains++;
                     }
                     int lastChain = A[i];
-                    for (int j = i+1; j < A.Length-1; j++)
+                    for (int j = i + 1; j < A.Length - 1; j++)
                     {
                         if (Math.Abs(lastChain - A[j]) <= 3 && Math.Abs(lElm - A[j]) <= 3)
                         {
@@ -48,6 +51,6 @@ namespace CountThree
             }
             return T.Sum();
         }
-        
+
     }
 }
