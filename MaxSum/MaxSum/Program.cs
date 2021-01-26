a5b21614081b232a227673c63ec8f2c3b4f214d6
Savage <crsavage0630@gmail.com>
2021-01-25 22:18:01 -0500

    maxsum

--- MaxSum/MaxSum/Program.cs
+++ MaxSum/MaxSum/Program.cs
@@ -7,8 +7,9 @@ namespace MaxSum
     {
         static void Main(string[] args)
         {
-            int[] sequence = new int[] { 5, 8, 3, 7, 9, 1 };
-            // Answer is 3: 5,7,9
+            //int[] sequence = new int[] { 5, 8, 3, 7, 9, 1 };
+            // Answer is 19: 3+7+9
+            int[] sequence = new int[] { 5, 2, 4, 6, 9, 3 };
             Console.WriteLine(MaxSum(sequence));
         }
 
@@ -19,13 +20,17 @@ namespace MaxSum
             for (int i = 0; i < sequence.Length; i++)
 			{
                 T[i] = sequence[i];
-                for(int j = 0; j <= i; j++)
+                for(int j = 0; j < i; j++)
                 {
 
                     Console.WriteLine("j position: "+sequence[j]);
                     if(sequence[j] < sequence[i])
                     {
                         T[i] = sequence[i] +Math.Max(T[i-1], 0);
+                    }
+                    else
+                    {
+                        T[i] = sequence[i];
                     }
                 }
 			}
